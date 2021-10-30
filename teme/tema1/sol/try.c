c2e8e6123740cbf3f874c5d07b66c1de7618a2e4
Maria <mariamosneag@gmail.com>
2021-10-31 00:51:11 +0300

    update

---
+++ teme/tema1/sol/try.c
@@ -0,0 +1,118 @@
+#include <stdlib.h>
+#include "genetic_algorithm.h"
+
+void do_magic(sack_object *objects, int object_count, int generations_count, int sack_capacity) {
+    int count, cursor;
+	individual *current_generation = (individual*) calloc(object_count, sizeof(individual));
+	individual *next_generation = (individual*) calloc(object_count, sizeof(individual));
+	individual *tmp = NULL;
+
+	// set initial generation (composed of object_count individuals with a single item in the sack)
+	for (int i = 0; i < object_count; ++i) {
+		current_generation[i].fitness = 0;
+		current_generation[i].chromosomes = (int*) calloc(object_count, sizeof(int));
+		current_generation[i].chromosomes[i] = 1;
+		current_generation[i].index = i;
+		current_generation[i].chromosome_length = object_count;
+
+		next_generation[i].fitness = 0;
+		next_generation[i].chromosomes = (int*) calloc(object_count, sizeof(int));
+		next_generation[i].index = i;
+		next_generation[i].chromosome_length = object_count;
+	}
+
+	// iterate for each generation
+	for (int k = 0; k < generations_count; ++k) {
+		cursor = 0;
+
+		// compute fitness and sort by it
+		compute_fitness_function(objects, current_generation, object_count, sack_capacity);
+		qsort(current_generation, object_count, sizeof(individual), cmpfunc);
+
+		// keep first 30% children (elite children selection)
+		count = object_count * 3 / 10;
+		for (int i = 0; i < count; ++i) {
+			copy_individual(current_generation + i, next_generation + i);
+		}
+		cursor = count;
+
+		// mutate first 20% children with the first version of bit string mutation
+		count = object_count * 2 / 10;
+		for (int i = 0; i < count; ++i) {
+			copy_individual(current_generation + i, next_generation + cursor + i);
+			mutate_bit_string_1(next_generation + cursor + i, k);
+		}
+		cursor += count;
+
+		// mutate next 20% children with the second version of bit string mutation
+		count = object_count * 2 / 10;
+		for (int i = 0; i < count; ++i) {
+			copy_individual(current_generation + i + count, next_generation + cursor + i);
+			mutate_bit_string_2(next_generation + cursor + i, k);
+		}
+		cursor += count;
+
+		// crossover first 30% parents with one-point crossover
+		// (if there is an odd number of parents, the last one is kept as such)
+		count = object_count * 3 / 10;
+
+		if (count % 2 == 1) {
+			copy_individual(current_generation + object_count - 1, next_generation + cursor + count - 1);
+			count--;
+		}
+
+		for (int i = 0; i < count; i += 2) {
+			crossover(current_generation + i, next_generation + cursor + i, k);
+		}
+
+		// switch to new generation
+		tmp = current_generation;
+		current_generation = next_generation;
+		next_generation = tmp;
+
+		for (int i = 0; i < object_count; ++i) {
+			current_generation[i].index = i;
+		}
+
+		if (k % 5 == 0) {
+			print_best_fitness(current_generation);
+		}
+	}
+
+	compute_fitness_function(objects, current_generation, object_count, sack_capacity);
+	qsort(current_generation, object_count, sizeof(individual), cmpfunc);
+	print_best_fitness(current_generation);
+
+	// free resources for old generation
+	free_generation(current_generation);
+	free_generation(next_generation);
+
+	// free resources
+	free(current_generation);
+	free(next_generation);
+}
+
+int main(int argc, char *argv[]) {
+	// array with all the objects that can be placed in the sack
+	sack_object *objects = NULL;
+
+	// number of objects
+	int object_count = 0;
+
+	// maximum weight that can be carried in the sack
+	int sack_capacity = 0;
+
+	// number of generations
+	int generations_count = 0;
+
+	if (!read_input(&objects, &object_count, &sack_capacity, &generations_count, argc, argv)) {
+		return 0;
+	}
+
+	// run_genetic_algorithm(objects, object_count, generations_count, sack_capacity);
+    do_magic(objects, object_count, generations_count, sack_capacity);
+
+	free(objects);
+
+	return 0;
+}
