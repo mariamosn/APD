72f96a1434b4ba24ded6d97ac12127a3269bd895
Maria <mariamosneag@gmail.com>
2021-11-06 00:04:21 +0200

    lab04

--- laboratoare/lab04/src/bug2/MyThread.java
+++ laboratoare/lab04/src/bug2/MyThread.java
@@ -2,6 +2,9 @@ package bug2;
 
 /**
  * Why does this code not block? We took the same lock twice!
+ *
+ * There is nothing to modify for this example. Just explain why we do NOT
+ * have a deadlock.
  */
 public class MyThread implements Runnable {
     static int i;
@@ -9,7 +12,10 @@ public class MyThread implements Runnable {
     @Override
     public void run() {
         synchronized (this) {
+            // aici poate ajunge un singur thread la un moment dat
             synchronized (this) {
+                // si aici tot un singur thread, dar oricum nu puteau ajunge mai multe in acelasi timp din cauza
+                // synchronized-ului anterior
                 i++;
             }
         }
--- laboratoare/lab04/src/shortestPathsFloyd_Warshall/Main.java
+++ laboratoare/lab04/src/shortestPathsFloyd_Warshall/Main.java
@@ -1,6 +1,7 @@
 package shortestPathsFloyd_Warshall;
 
 public class Main {
+    private final static int numberOfThreads = Runtime.getRuntime().availableProcessors();
 
     public static void main(String[] args) {
         int M = 9;
@@ -10,12 +11,17 @@ public class Main {
                 {M, M, 1, 0, M},
                 {M, M, 1, M, 0}};
 
-        // Parallelize me (You might want to keep the original code in order to compare)
-        for (int k = 0; k < 5; k++) {
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    graph[i][j] = Math.min(graph[i][k] + graph[k][j], graph[i][j]);
-                }
+        Thread[] threads = new Thread[numberOfThreads];
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads[i] = new MyThread(i, 5, numberOfThreads, graph);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
 
@@ -27,3 +33,10 @@ public class Main {
         }
     }
 }
+/*
+0 1 2 3 3
+1 0 1 2 2
+2 1 0 1 1
+3 2 1 0 2
+3 2 1 2 0
+ */
