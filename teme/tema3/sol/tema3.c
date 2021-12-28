2a11f1fda7c4b0a58f35940f0ea1b7851e636624
Maria <mariamosneag@gmail.com>
2021-12-28 21:27:00 +0200

    TODO: refactor code

--- teme/tema3/sol/tema3.c
+++ teme/tema3/sol/tema3.c
@@ -3,6 +3,7 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 // tipul procesului
 #define COORDINATOR 0
@@ -26,6 +27,10 @@ int main (int argc, char *argv[]) {
     int process_type;
 
     int num_of_workers[COORD_NUM], *workers[COORD_NUM];
+    int *v, size;
+
+    int my_coordinator;
+    int *offset;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
@@ -58,6 +63,7 @@ int main (int argc, char *argv[]) {
         }
 
         fscanf(in, "%d\n", &my_num_of_workers);
+        // TODO: de facut verificare la malloc
         my_workers = malloc(my_num_of_workers * sizeof(int));
         for (int i = 0; i < my_num_of_workers; i++) {
             fscanf(in, "%d\n", &my_workers[i]);
@@ -93,6 +99,7 @@ int main (int argc, char *argv[]) {
 
             MPI_Recv(&recv_num_of_workers, 1, MPI_INT, MPI_ANY_SOURCE, 0,
                         MPI_COMM_WORLD, &status);
+            // TODO: de facut verificare la malloc
             recv_workers = malloc(recv_num_of_workers * sizeof(int));
             MPI_Recv(recv_workers, recv_num_of_workers, MPI_INT,
                         status.MPI_SOURCE, 0, MPI_COMM_WORLD,
@@ -120,6 +127,7 @@ int main (int argc, char *argv[]) {
                 int src = 1 - rank;
                 MPI_Recv(&num_of_workers[src], 1, MPI_INT, 2, 0, MPI_COMM_WORLD,
                             MPI_STATUS_IGNORE);
+                // TODO: de facut verificare la malloc
                 workers[src] = malloc(num_of_workers[src] * sizeof(int));
                 MPI_Recv(workers[src], num_of_workers[src], MPI_INT, 2, 0,
                             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -148,7 +156,7 @@ int main (int argc, char *argv[]) {
 
     } else if (process_type == WORKER) {
         // află coordonatorul
-        int my_coordinator;
+        // int my_coordinator;
 
         MPI_Recv(&my_coordinator, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
                     MPI_STATUS_IGNORE);
@@ -156,6 +164,7 @@ int main (int argc, char *argv[]) {
         for (int i = 0; i < COORD_NUM; i++) {
             MPI_Recv(&num_of_workers[i], 1, MPI_INT, my_coordinator, 0,
                         MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // TODO: de facut verificare la malloc
             workers[i] = malloc(num_of_workers[i] * sizeof(int));
             MPI_Recv(workers[i], num_of_workers[i], MPI_INT, my_coordinator, 0,
                         MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -173,13 +182,209 @@ int main (int argc, char *argv[]) {
     }
     printf("\n");
 
+    MPI_Barrier(MPI_COMM_WORLD);
+
     ////////////////////////////////////////////////////////////////////////
 
     // realizarea calculelor
 
+    if (process_type == COORDINATOR) {
+        // int *offset;
+        if (rank == 0) {
+            // generarea vectorului
+            // TODO: de facut verificare la malloc
+            v = malloc(n * sizeof(int));
+            for (int i = 0; i < n; i++) {
+                v[i] = i;
+            }
+
+            // TODO: de facut verificare la malloc
+            offset = malloc(COORD_NUM * sizeof(int));
+
+            // împărțirea vectorului între coordonatori
+            int total_num_of_workers = numtasks - COORD_NUM;
+            size = (double)n / total_num_of_workers * num_of_workers[0];
+            offset[0] = 0;
+            offset[1] = offset[0] + size;
+
+            for (int i = 1; i < COORD_NUM; i++) {
+                int dest = i;
+                if (comm_status == ERROR && i == 1) {
+                    dest = 2;
+                }
+
+                int crt_size = (double)n / total_num_of_workers *
+                                num_of_workers[i];
+                if (i == 2 && offset[i] + crt_size != n) {
+                    crt_size = n - offset[i];
+                }
+
+                MPI_Send(&crt_size, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                printf("M(%d,%d)\n", rank, dest);
+
+                MPI_Send(v + offset[i], crt_size, MPI_INT, dest, 0,
+                            MPI_COMM_WORLD);
+                printf("M(%d,%d)\n", rank, dest);
+
+                if (i < COORD_NUM - 1) {
+                    offset[i + 1] = offset[i] + crt_size;
+                }
+            }
+        } else if (comm_status == CONNECTED ||
+                    comm_status == ERROR && rank == 1) {
+            // preia datele de la sursă
+            MPI_Recv(&size, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
+                        MPI_STATUS_IGNORE);
+            // TODO: de facut verificare la malloc
+            v = malloc(size * sizeof(int));
+            MPI_Recv(v, size, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
+                        MPI_STATUS_IGNORE);
+        } else if (comm_status == ERROR && rank == 2) {
+            // intermediază comunicarea dintre 0 și 1
+
+            // primește de la 0 datele lui 1
+            int *aux_v, aux_size;
+            MPI_Recv(&aux_size, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
+                        MPI_STATUS_IGNORE);
+            // TODO: de facut verificare la malloc
+            aux_v = malloc(aux_size * sizeof(int));
+            MPI_Recv(aux_v, aux_size, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
+                        MPI_STATUS_IGNORE);
+
+            // trimite către 1
+            MPI_Send(&aux_size, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            printf("M(%d,%d)\n", rank, 1);
+
+            MPI_Send(aux_v, aux_size, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            printf("M(%d,%d)\n", rank, 1);
+            free(aux_v);
+
+            // preia datele de la procesul 0
+            MPI_Recv(&size, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
+                        MPI_STATUS_IGNORE);
+            // TODO: de facut verificare la malloc
+            v = malloc(size * sizeof(int));
+            MPI_Recv(v, size, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
+                        MPI_STATUS_IGNORE);
+        }
+
+        // trimite către workeri partea corespunzătoare
+        int default_send_size = ceil((double)size / num_of_workers[rank]);
+        for (int i = 0; i < num_of_workers[rank]; i++) {
+            int off = i * default_send_size;
+            int sz = default_send_size;
+            if (off + sz > size || i == num_of_workers[rank] - 1) {
+                sz = size - off;
+            }
+
+            MPI_Send(&sz, 1, MPI_INT, workers[rank][i], 0, MPI_COMM_WORLD);
+            printf("M(%d,%d)\n", rank, workers[rank][i]);
+
+            MPI_Send(v + off, sz, MPI_INT, workers[rank][i], 0, MPI_COMM_WORLD);
+            printf("M(%d,%d)\n", rank, workers[rank][i]);
+        }
+
+        //*****************************************************************//
+
+        // primește de la workeri părțile modificate
+        for (int i = 0; i < num_of_workers[rank]; i++) {
+            int off = i * default_send_size;
+            int sz = default_send_size;
+            if (off + sz > size || i == num_of_workers[rank] - 1) {
+                sz = size - off;
+            }
+
+            MPI_Recv(v + off, sz, MPI_INT, workers[rank][i], 0, MPI_COMM_WORLD,
+                        MPI_STATUS_IGNORE);
+        }
+
+        // 0 preia vectorii de la ceilalți coordonatori
+        if (comm_status == CONNECTED) {
+            if (rank == 0) {
+                for (int i = 1; i < COORD_NUM; i++) {
+                    int sz;
+                    if (i == 1) {
+                        sz = offset[i + 1] - offset[i];
+                    } else {
+                        sz = n - offset[i];
+                    }
+                    MPI_Recv(v + offset[i], sz, MPI_INT, i, 0, MPI_COMM_WORLD,
+                        MPI_STATUS_IGNORE);
+                }
+            } else {
+                MPI_Send(v, size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                printf("M(%d,%d)\n", rank, 0);
+            }
+        } else {
+            if (rank == 0) {
+                MPI_Recv(v + offset[2], n - offset[2], MPI_INT, 2, 0,
+                            MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(v + offset[1], offset[2] - offset[1], MPI_INT, 2, 0,
+                            MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            } else if (rank == 1) {
+                MPI_Send(&size, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+                printf("M(%d,%d)\n", rank, 2);
+
+                MPI_Send(v, size, MPI_INT, 2, 0, MPI_COMM_WORLD);
+                printf("M(%d,%d)\n", rank, 2);
+            } else {
+                MPI_Send(v, size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                printf("M(%d,%d)\n", rank, 0);
+
+                int sz;
+                MPI_Recv(&sz, 1, MPI_INT, 1, 0, MPI_COMM_WORLD,
+                            MPI_STATUS_IGNORE);
+                int *aux_v = malloc(sz * sizeof(int));
+                MPI_Recv(aux_v, sz, MPI_INT, 1, 0, MPI_COMM_WORLD,
+                            MPI_STATUS_IGNORE);
+
+                MPI_Send(aux_v, sz, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                printf("M(%d,%d)\n", rank, 0);
+
+                free(aux_v);
+            }
+        }
+
+        // 0 afișează rezultatele
+        if (rank == 0) {
+            printf("Rezultat: ");
+            for (int i = 0; i < n; i++) {
+                printf("%d ", v[i]);
+            }
+            printf("\n");
+        }
+
+    } else if (process_type == WORKER) {
+        // primește partea de vector
+        MPI_Recv(&size, 1, MPI_INT, my_coordinator, 0, MPI_COMM_WORLD,
+                    MPI_STATUS_IGNORE);
+        v = malloc(size * sizeof(int));
+        MPI_Recv(v, size, MPI_INT, my_coordinator, 0, MPI_COMM_WORLD,
+                    MPI_STATUS_IGNORE);
+
+        // modifică vectorul corespunzător (x2)
+        for (int i = 0; i < size; i++) {
+            v[i] = v[i] << 1;
+        }
+
+        // trimite vectorul modificat coordonatorului
+        MPI_Send(v, size, MPI_INT, my_coordinator, 0, MPI_COMM_WORLD);
+        printf("M(%d,%d)\n", rank, my_coordinator);
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // eliberarea memoriei
+    for (int i = 0; i < COORD_NUM; i++) {
+        free(workers[i]);
+    }
+    free(v);
     
+    if (rank == 0) {
+        free(offset);
+    }
 
     MPI_Finalize();
-
 }
-
