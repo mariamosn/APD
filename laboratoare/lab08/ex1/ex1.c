11c5a05a18d2de1aa6fba25ba74ec3636478372a
Maria <mariamosneag@gmail.com>
2022-01-20 00:51:06 +0200

    lab08 fix

--- laboratoare/lab08/ex1/ex1.c
+++ laboratoare/lab08/ex1/ex1.c
@@ -21,13 +21,15 @@ int main (int argc, char *argv[])
         printf("The initial value is: %d.\n", rand_val);
         MPI_Send(&rand_val, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
         MPI_Recv(&recv_num, 1, MPI_INT, numtasks - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
+        printf("Process %d received %d.\n", rank, recv_num);
+        
     } else if (rank == numtasks - 1) {
         // Last process close the circle.
         // Receives the number from the previous process.
         // Increments the number.
         // Sends the number to the first process.
         MPI_Recv(&recv_num, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process %d received %d.\n", rank, recv_num);
         recv_num += 2;
         MPI_Send(&recv_num, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 
@@ -37,11 +39,10 @@ int main (int argc, char *argv[])
         // Increments the number.
         // Sends the number to the next process.
         MPI_Recv(&recv_num, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process %d received %d.\n", rank, recv_num);
         recv_num += 2;
         MPI_Send(&recv_num, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
     }
-
-    printf("Process %d received %d.\n", rank, recv_num);
 
     MPI_Finalize();
 
